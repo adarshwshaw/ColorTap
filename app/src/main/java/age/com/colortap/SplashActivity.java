290724b59bc2ff953aad87b3372363728b812dd6
adarshwshaw <adarshwshaw@gmail.com>
2017-02-04 00:03:33 +0530

    change orientation,added imageview to splash activity

--- app/src/main/java/age/com/colortap/SplashActivity.java
+++ app/src/main/java/age/com/colortap/SplashActivity.java
@@ -10,8 +10,8 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
     }
 }
