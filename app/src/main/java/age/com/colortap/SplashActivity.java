6e0865b013112292c8fec66f01052a960292e4ca
adarshwshaw <adarshwshaw@gmail.com>
2017-02-03 23:36:49 +0530

    Full Screen in Splash Activity

--- app/src/main/java/age/com/colortap/SplashActivity.java
+++ app/src/main/java/age/com/colortap/SplashActivity.java
@@ -2,6 +2,8 @@ package age.com.colortap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,7 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
