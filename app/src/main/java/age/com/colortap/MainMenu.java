e54472b5325e4e4ca894c1e228d86c34eb120a02
adarshwshaw <adarshwshaw@gmail.com>
2017-02-04 21:20:38 +0530

    Splash Activity completed

---
+++ app/src/main/java/age/com/colortap/MainMenu.java
@@ -0,0 +1,17 @@
+package age.com.colortap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
